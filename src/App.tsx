68b6d689278cf04585d91072fec5649f60d5f233
Eduard Koshkelyan <eduardko2001@gmail.com>
2023-12-06 19:33:18 +0400

    feat(inventory-table): add showing items into table

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,51 @@
 import { useState, useEffect } from "react";
+
+import { Space, Table, Tag } from "antd";
+import type { ColumnsType } from "antd/es/table";
+
 import Header from "./components/Header";
 
+interface DataType {
+  key: number;
+  name: string;
+  category: string;
+  connector: { url: string; alt: string };
+}
+
+interface InventoryType {
+  appId: string;
+  appName: string;
+  appSources: string[];
+  category: string;
+}
+
+const columns: ColumnsType<DataType> = [
+  {
+    title: "Name",
+    dataIndex: "name",
+    key: "name",
+    sorter: (a, b) => a.name.length - b.name.length,
+  },
+  {
+    title: "Category",
+    dataIndex: "category",
+    key: "category",
+    sorter: (a, b) => a.name.length - b.name.length,
+  },
+  {
+    title: "Connector",
+    dataIndex: "connector",
+    key: "connector",
+    render: ({ url, alt }) => <img src={url} alt={alt} />,
+    sorter: (a, b) => a.name.length - b.name.length,
+  },
+];
+
 function App() {
-  const [inventory, setInventory] = useState([]);
+  const [inventory, setInventory] = useState<DataType[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  const [pageSize, setPageSize] = useState(25);
 
   useEffect(() => {
     const getInventory = async () => {
@@ -19,12 +62,28 @@ function App() {
             "ngrok-skip-browser-warning": "69420",
           },
         });
-        const fetchedInventory = res.json();
+        const fetchedInventory: Awaited<{
+          appRows: InventoryType[];
+          totalCount: number;
+        }> = await res.json();
+
+        const mappedRows: DataType[] = fetchedInventory.appRows.map(
+          (row, idx) => ({
+            key: idx + 1,
+            name: row.appName,
+            category: row.category,
+            connector: { url: "/images/logo.svg", alt: row.appSources[0] },
+          })
+        );
+
+        setInventory(mappedRows);
 
         console.log(fetchedInventory);
       } catch (error) {
         console.error(error);
       }
+
+      setLoading(false);
     };
 
     getInventory();
@@ -33,6 +92,24 @@ function App() {
   return (
     <>
       <Header />
+      <div>
+        <h1>App Inventory</h1>
+        <Table
+          columns={columns}
+          dataSource={inventory}
+          loading={loading}
+          pagination={{
+            onChange: () => {},
+            onShowSizeChange: (prop1, prop2) => {
+              console.log(prop1, prop2);
+            },
+            pageSize,
+            pageSizeOptions: ["25", "50"],
+            showSizeChanger: true,
+          }}
+        />
+        ;
+      </div>
     </>
   );
 }
